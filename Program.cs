e432685e2ae566dda784801196389bb3a29d2f36
Eicha2115 <fahridm741@gmail.com>
2026-04-01 09:02:57 +0700

    Lanjutan Inheritance dan membuat turunan

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿public class user
+﻿Siswa siswaObj = new Siswa("Fahri", "Fahridm@gmail.com", "1115", "Pelajar/Siswa", "X PG 1");
+Guru guruObj = new Guru("Fahra", "Fahramd@gmail.com", "5111", "Pengajar/Guru", "Bahasa india");
+
+siswaObj.TampilkanInfo();
+siswaObj.SubmitTugas();
+Console.WriteLine();
+guruObj.TampilkanInfo();
+guruObj.NilaiTugas();
+
+public class user
 {
     public string nama;
     public string email;
@@ -14,7 +23,37 @@
     }
     public void TampilkanInfo()
     {
-        Console.WriteLine($"Name: {nama} | Role: {role}");
+        Console.WriteLine($"Name: {nama} | Role: {role} ");
     }
     
+}
+
+class Siswa : user
+{
+    public string ClassName;
+
+    public Siswa(string nama, string email, string password, string role, string className) : base(nama, email, password, role)
+    {
+        ClassName = className;
+    }
+
+    public void SubmitTugas()
+    {
+        Console.WriteLine($"{nama} dari kelas {ClassName} sudah mengumpulkan tugas!");
+    }
+}
+
+class Guru : user
+{
+    public string Mapel;
+
+    public Guru(string nama, string email, string password, string role, string mapel) : base(nama, email, password, role)
+    {
+        Mapel = mapel;
+    }
+
+    public void NilaiTugas()
+    {
+        Console.WriteLine($"Guru {nama} telah menilai semua tugas mapel {Mapel}.");
+    }
 }
